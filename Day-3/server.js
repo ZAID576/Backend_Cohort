df6ac65d8cfc51e7e44f65723d3a57e9e7b875c7
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-08-27 13:32:53 +0530

    adding GET

--- Day-3/server.js
+++ Day-3/server.js
@@ -8,8 +8,13 @@ const notes = []
 
 app.post("/notes",(req,res)=>{
     console.log(req.body)
+    notes.push(req.body)
     res.send("note created")
 }) //API 
+
+app.get("/notes", (req,res)=>{
+     res.send(notes)
+}) //API
   
 app.listen(3000,()=>{
     console.log("Server is running on port 3000")
