3c95fbe0730086b88fb15b9aedde11e188066186
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-08-28 02:12:41 +0530

    feat: implement CRUD operations for notes in app.js

--- Day-4/src/app.js
+++ Day-4/src/app.js
@@ -7,4 +7,28 @@ const express = require("express")
 
 const app = express() // server created
 
+app.use(express.json())
+
+const notes = []
+
+
+// app.get("/",(req,res)=>{
+//    res.send("Hello World")
+// })
+
+app.post("/notes", (req,res)=>{
+  console.log(req.body)
+  console.log(notes)
+  notes.push(req.body)
+  res.send("notes send")
+})
+
+app.get("/notes",(req,res)=>{
+  res.send(notes)
+})
+
+app.delete("/notes/:index",(req,res)=>{
+  console.log(req.params.index)
+})
+
 module.exports = app
