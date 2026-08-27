82b0aea153aa85a1351ce54f8f37460492a14e85
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-08-28 00:38:35 +0530

    add app.js with server setup and configuration

---
+++ Day-4/src/app.js
@@ -0,0 +1,10 @@
+//app.js file ka 2 kaam hota hai
+// 1st -  server create kerna
+// 2nd -  server ko config kerna
+
+
+const express = require("express")
+
+const app = express() // server created
+
+module.exports = app
