edeac8ff056fb17abb5f9a47ba4d56e3d536e247
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-08-27 13:01:38 +0530

    adding middleware

--- Day-3/server.js
+++ Day-3/server.js
@@ -1,16 +1,16 @@
 const express = require("express")
 
-const app = express()
+const app = express() // server create
 
-app.get("/", (req, res)=>{
-    res.send("Hello World")
+app.use(express.json()) // Middleware
+
+const notes = []
+
+app.post("/notes",(req,res)=>{
+    console.log(req.body)
+    res.send("note created")
 })
-
-app.get ( "/Home",(req,res) => {
-    res.send("This is Home")
-})
-
-
-app.listen(3000, ()=>{
+  
+app.listen(3000,()=>{
     console.log("Server is running on port 3000")
-}); 
+})  // server start
