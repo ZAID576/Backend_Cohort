6672c99af57e72a6d77db7fd9543edda8cad2ff3
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-08-28 16:18:25 +0530

    PATCH

--- Day-4/src/app.js
+++ Day-4/src/app.js
@@ -27,10 +27,23 @@ app.get("/notes",(req,res)=>{
   res.send(notes)
 })
 
+
+// DELETE /notes
+// delete /notes/0
+
 app.delete("/notes/:index",(req,res)=>{
   delete notes[req.params.index]
 
   res.send("note deleted successfully")
 })
+
+// PATCH /notes/:index
+// req.body = {description :- "sample modified description."}
+app.patch("/notes/:index",(req,res)=>{
+  notes[req.params.index].description = req.body.description
+  notes[req.params.index].titles = req.body.titles
+  res.send("note updated successfully")
+})
+
 
 module.exports = app
