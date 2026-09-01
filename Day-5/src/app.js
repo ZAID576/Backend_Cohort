76871ce7e61d344a398aef07eef588833c2dd642
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-09-02 00:02:25 +0530

    CRUD using postman

--- Day-5/src/app.js
+++ Day-5/src/app.js
@@ -12,6 +12,35 @@ const notes = []
 // POST 
 app.post("/notes",(req,res)=>{
   notes.push(req.body)
+
+ res.status(201).json({
+   message : "Note created successfully"
+ })
+})
+
+// GET
+app.get("/notes", (req,res)=>{
+    res.status(200).json({
+        notes : notes
+    })
+})
+
+// DELETE 
+app.delete("/notes/:index",(req,res)=>{
+   delete notes [req.params.index]
+
+   res.status(204).json({
+     message : "Note deleted successfully"
+   })
+})
+
+// PATCH
+app.patch("/notes/:index",(req,res)=>{
+    notes[req.params.index].description = req.body.description
+
+    res.status(200).json({
+        message : "Note updated successfully"
+    })
 })
 
 module.exports = app
