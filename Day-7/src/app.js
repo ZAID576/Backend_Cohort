1bec8d28d1d4f66b68c3074d5ce80bd8f8d3ed43
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-09-03 00:56:25 +0530

    get

--- Day-7/src/app.js
+++ Day-7/src/app.js
@@ -8,17 +8,29 @@ const app = express()
 
 app.use(express.json())
 
+// post
 app.post("/notes",async (req,res)=>{
    const { title , description } = req.body
 
-   const note = await noteModel.create({
+   const notes  = await noteModel.create({
     title,description
    })
 
    res.status(201).json({
     message : "Note created successfully",
-    note
+    notes
    })
+})
+
+//get
+app.get("/notes",async (req,res)=>{
+    const notes = await noteModel.find()
+    
+    res.status(200).json({
+    message : "Note fetch successfully",
+    notes
+   })
+    
 })
 
 module.exports = app 
