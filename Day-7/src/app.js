874efe4b713c6fd5ec79f15d881dd25b83bfd8d6
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-09-02 23:58:56 +0530

    Add POST endpoint for notes creation in app.js

--- Day-7/src/app.js
+++ Day-7/src/app.js
@@ -2,7 +2,18 @@
 // server ko config kerna 
 
 const express = require("express")
+const noteModel = require("./model/notes.noteModel")
 
 const app = express()
+
+app.use(express.json())
+
+app.post("/notes",(req,res)=>{
+   const { title , description } = req.body
+
+   noteModel.create({
+    title,description
+   })
+})
 
 module.exports = app 
