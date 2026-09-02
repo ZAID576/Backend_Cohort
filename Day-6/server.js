cf94b88dc7af45e07f88deba8f26cc6290b348d8
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-09-02 20:17:02 +0530

    server connected to database

--- Day-6/server.js
+++ Day-6/server.js
@@ -3,7 +3,18 @@
 
 
 const app = require("./src/app")
+const mongoose = require("mongoose")
+
+function connectToDb(){
+    mongoose.connect("mongodb+srv://zkonline576_db_user:yEwCCC8tNWciPlZQ@cluster0.j0sggcl.mongodb.net/Day-6")
+    .then(()=>{
+        console.log("Connected to Database")
+    })
+} 
+
+connectToDb() // server connect
+
 
 app.listen(3000,(req,res)=>{
     console.log("Server is running on port 3000")
-})
+}) //server start
