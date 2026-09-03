120d965d79264fd60a62b0b896df9da21e8a3b69
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-09-03 21:40:54 +0530

    // POST

--- Day-8/Backend/src/app.js
+++ Day-8/Backend/src/app.js
@@ -2,7 +2,26 @@
 // server ko config kerna
 
 const express = require("express")
+const noteModel = require("./Models/note.model")
 
 const app = express()
+
+app.use(express.json())
+
+//POST
+app.post("/api/notes", async (req,res)=>{
+    const {title , description} = req.body
+
+    const note = await noteModel.create({
+        title,description
+    })
+
+    res.status(201).json({
+        message : "note created successfully",
+        note
+    })
+})
+
+
 
 module.exports = app
