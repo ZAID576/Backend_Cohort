9e89e4c270b2f390ffc0d0e44ae5d29653228568
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-09-11 12:00:14 +0530

    fix: resolve merge conflicts in server.js files

--- Day-2/server.js
+++ Day-2/server.js
@@ -1,10 +1,6 @@
 const express = require("express")
 
-<<<<<<< HEAD
 const app = express() // server created
-=======
-const app = express()
->>>>>>> 15f0dfc425127656753c4edacdfe677f58f692fd
 
 app.get("/", (req, res)=>{
     res.send("Hello World")
@@ -17,8 +13,8 @@ app.get ( "/Home",(req,res) => {
 
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000")
-<<<<<<< HEAD
+
 });  // server started
-=======
-}); 
->>>>>>> 15f0dfc425127656753c4edacdfe677f58f692fd
+
+
+
